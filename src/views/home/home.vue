f0d3f3fbd7ba772bd99b7a0bfb7f0be859cdaa36
210923729@qq.com <210923729@qq.com>
2022-06-29 18:24:01 +0800

    主页基本布局

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    首页
-  </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="160px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.el-container {
+  width: 100vw;
+  height: 100vh;
+  .el-header {
+    background-color: #B0C4DE;
+  }
+  .el-container {
+    .el-aside {
+      background-color: #B0C4DE;
+      // background-image: linear-gradient(to bottom right, #5EFCE8 , #736EFE);
+    }
+    .el-main {
+      padding: 10px;
+      background-color: #E6E6FA;
+    }
+  }
+}
 </style>
